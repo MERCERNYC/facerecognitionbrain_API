2f06ff27313615f1261955cc55494e2283d7355c
Juliana Fogg <julianafogg@gmail.com>
2021-01-08 11:42:04 -0500

    add knex to connect database

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+
+const db = knex ({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'julianafogg',
+      password : '',
+      database : 'smart_brain'
+    }
+  });
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 
 const database = { 
@@ -24,6 +39,13 @@ const database = {
             entries: 0,
             joined: new Date()
         }
+    ],
+    login:[
+        {
+            id:'987',
+            hash:'',
+            email:'john@gmail.com'
+        }
     ]
 }
 
@@ -34,7 +56,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
    if(req.body.email === database.users[0].email && 
     req.body.password === database.users[0].password) {
-        res.json('success');
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in')
     }
@@ -42,16 +64,16 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
+    db('users')
+      .returning('*')
+      .insert({
+        email:email, 
+        name:name,
         joined: new Date()
-
     })
-    res.json(database.users[database.users.length-1]);
+     .then(response => {
+        res.json(response);
+     })    
 })
 
 app.get('/profile/:id', (req, res) => {
@@ -60,13 +82,26 @@ app.get('/profile/:id', (req, res) => {
     database.users.forEach(user => {
         if(user.id === id) {
           found = true;
-          return res.json(user)
-        } else {
-           res.status(404).json('no such user');
+          return res.json(user);
         }
     })
     if (!found) {
-        res.status(400)
+        res.status(400).json('not found')
+    }
+})
+
+app.put('/image', (req, res) => {
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+          found = true;
+          user.entries++
+          return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found')
     }
 })
 
@@ -74,9 +109,3 @@ app.listen(3000, ()=> {
     console.log("app is running on port 3000");
 })
 
-/*
-
-/register--POST = user
-/profile/:user:ID --GET = user
-/image -- PUT = update user 
-*/
