80688048ff72730746a4ebb00cd35a40bab4e7d8
Juliana Fogg <julianafogg@gmail.com>
2021-01-08 22:49:55 -0500

    add trx and bcrypt to register

--- server.js
+++ server.js
@@ -65,45 +65,54 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body;
-    db('users')
-      .returning('*')
-      .insert({
-        email:email, 
-        name:name,
-        joined: new Date()
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email,
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+          .returning('*')
+          .insert({
+            email:loginEmail[0], 
+            name:name,
+            joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0]);
+        })    
     })
-     .then(user => {
-        res.json(user[0]);
-     })    
-     .catch(err => res.status(400).json('unable to register'))
+    .then(trx.commit)
+    .catch(trx.rollback)
+})
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
     const {id} = req.params;
-    let found = false;
-    db.select('*').from ('users').where({
-        id: id
-    }).then(user => {
-     console.log(user);
+    db.select('*').from ('users').where({id})
+       .then(user => {
+        if (user.length) {
+          res.json(user[0])
+        } else {
+          res.status(400).json('user not found')
+        }   
     })
-    if (!found) {
-        res.status(400).json('not found')
-    }
+    .catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
     const {id} = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-          found = true;
-          user.entries++
-          return res.json(user.entries);
-        }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
     })
-    if (!found) {
-        res.status(400).json('not found')
-    }
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3000, ()=> {
