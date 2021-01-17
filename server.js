2d6228fceefdb063511fa631c2bd9d403302f9c2
Juliana Fogg <julianafogg@gmail.com>
2021-01-17 12:24:14 -0500

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex ({
      // connect to your own database here   
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-fluffy-90418',
       user : 'julianafogg',
       password : '',
       database : 'smart_brain'
