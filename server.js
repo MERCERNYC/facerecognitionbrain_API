2f401304e44312050e13be6a5086ab982ceb8f14
Juliana Fogg <julianafogg@gmail.com>
2021-01-18 22:25:11 -0500

    fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.get('/', (req, res) => { res.send('its working!')})
+app.get('/', function (req, res) { res.send('its working!'); })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 
