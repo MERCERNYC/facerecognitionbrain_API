f6f67d5004839c17c8dc7767771a1e95240b219b
Juliana Fogg <julianafogg@gmail.com>
2021-01-19 10:56:06 -0500

    fix database

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
   const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-    }
+    // connect to your own database here
+  client: 'pg',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const app = express();
@@ -22,7 +24,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.get('/', function (req, res) { res.send('its working!'); })
+app.get('/', (req, res)=> { res.send('it is working') })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 
