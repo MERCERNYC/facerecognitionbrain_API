7f2bc279779411546b75d3dae5f306b0fbb0e475
Juliana Fogg <julianafogg@gmail.com>
2021-01-18 19:45:09 -0500

    fix

--- server.js
+++ server.js
@@ -9,19 +9,15 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-
- // connect to your own database here   
-const db = knex ({
+  const db = knex({
     client: 'pg',
     connection: {
-      ssl: {
-        rejectUnauthorized: false
-      }
-    } 
-  });
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    }
+});
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -39,6 +35,4 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`app is running on port ${process.env.PORT}`);
-})
+app.listen(process.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}`) })
